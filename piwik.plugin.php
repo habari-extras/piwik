104bbd15445653fe4f03d1692efd9d82e0deb6e5
Matt Read <matt@mattread.com>
2012-11-27 18:55:29 +0000

    add some dashboard modules to diasplay stat graphs (visitors, countries, browser). More will be added later.

--- piwik.plugin.php
+++ piwik.plugin.php
@@ -15,6 +15,7 @@ class Piwik extends Plugin
 			$form = new FormUI('piwik');
 			$form->append( 'text', 'siteurl', 'option:piwik__siteurl', _t('Piwik site URL', 'piwik') );
 			$form->append( 'text', 'sitenum', 'option:piwik__sitenum', _t('Piwik site number', 'piwik') );
+			$form->append( 'text', 'auth_token', 'option:piwik__auth_token', _t('Piwik Auth Token', 'piwik') );
 			$form->append( 'checkbox', 'trackloggedin', 'option:piwik__trackloggedin', _t( 'Track logged-in users', 'piwik' ) );
 			$form->append( 'submit', 'save', 'Save' );
 			$form->on_success( array($this, 'save_config') );
@@ -46,6 +47,54 @@ class Piwik extends Plugin
 		Options::set('piwik__trackloggedin', false);
 	}
 
+	public function action_init()
+	{
+		$this->add_template( 'dashboard.block.piwik_visitors', __DIR__ . '/dashboard.block.piwik.php' );
+		$this->add_template( 'dashboard.block.piwik_browsers', __DIR__ . '/dashboard.block.piwik.php' );
+		$this->add_template( 'dashboard.block.piwik_countries', __DIR__ . '/dashboard.block.piwik.php' );
+	}
+
+	/**
+	* Return a list of blocks that can be used for the dashboard
+	* @param array $block_list An array of block names, indexed by unique string identifiers
+	* @return array The altered array
+	*/
+	public function filter_dashboard_block_list($block_list)
+	{
+		$block_list['piwik_visitors'] = _t( 'Piwik Visitors', 'piwik');
+		$block_list['piwik_browsers'] = _t( 'Piwik Browsers', 'piwik');
+		$block_list['piwik_countries'] = _t( 'Piwik Countries', 'piwik');
+		return $block_list;
+	}
+
+	public function action_block_content_piwik_visitors($block, $theme)
+	{
+		$block->url = $this->get_block_url("&method=ImageGraph.get&apiModule=VisitsSummary&apiAction=get&graphType=evolution&period=day&date=previous30");
+	}
+
+	public function action_block_content_piwik_browsers($block, $theme)
+	{
+		$block->url = $this->get_block_url("&method=ImageGraph.get&apiModule=UserSettings&apiAction=getBrowser&graphType=horizontalBar&period=month&date=today");
+	}
+	
+	public function action_block_content_piwik_countries($block, $theme)
+	{
+		$block->url = $this->get_block_url("&method=ImageGraph.get&apiModule=UserCountry&apiAction=getCountry&graphType=verticalBar&period=month&date=today");
+	}
+
+	private function get_block_url($query)
+	{
+		$siteurl = Options::get('piwik__siteurl');
+        if ( $siteurl{strlen($siteurl)-1} != '/' ) {
+            $siteurl .= '/';
+        }
+        $ssl_siteurl = str_replace("http://", "https://", $siteurl);
+        $sitenum = Options::get('piwik__sitenum');
+		$auth_token = Options::get('piwik__auth_token');
+		
+		return "{$siteurl}index.php?module=API&idSite={$sitenum}&token_auth={$auth_token}{$query}&width=420&height=200";
+	}
+
 	public function theme_footer( Theme $theme )
 	{
 		// trailing slash url
@@ -73,7 +122,8 @@ LMNO;
         $tags = '';
         if ( count($theme->posts) == 1 && $theme->posts instanceof Post ) {
             foreach($theme->posts->tags as $i => $tag){
-                $tags .= "piwikTracker.setCustomVariable ({$i}, 'Tag', '{$tag->term_display}', 'page');";
+                $n = $i + 1;
+                $tags .= "piwikTracker.setCustomVariable ({$n}, 'Tag', '{$tag->term_display}', 'page');";
             }
         }
 		echo <<<EOD
